48decb7656e3685f489642dc64f6ee0614bc1831
Michele Tagliabue <mikitg.michele@gmail.com>
2019-11-04 10:56:22 +0100

    Corretto benchmark (divisione per numero di iterazioni di tipologia + migrazione su iterazioni pari)

--- src/minimal_overlay/bundles/migration/migration.c
+++ src/minimal_overlay/bundles/migration/migration.c
@@ -17,7 +17,12 @@ void do_computation(int* array, int array_length) {
         const int y = rand() % array_length;
         array[j] = (array[x] * array[y]) % array_length;
     }
+}
 
+void init_array(int* array, int array_length) {
+    for(int i=0; i<array_length; i++) {
+        array[i] = 1;
+    }
 }
 
 int get_CPU_number() {
@@ -45,25 +50,31 @@ int main(int argc, char *argv[]) {
     long i =0;
     const long iterations = atoi(argv[1]);
     const long initial_computation_complexity = atoi(argv[2]);
+
     for(int complexity_factor = 1; complexity_factor <= 6; complexity_factor++) {
         const int computation_complexity = pow(initial_computation_complexity,complexity_factor); 
 
-        printf("\n\nIterazioni: %ld, complessita: %d \n", iterations, computation_complexity);
+        printf("------\nIterazioni: %ld, complessita: %d", iterations, computation_complexity);
+
+        int migration_iterations = 0;
+        int normal_iterations = 0;
 
         uint64_t total_sequential_delays = 0;
         uint64_t total_migration_delays = 0;
         uint64_t delta_us;
 
         int array[computation_complexity];
+        init_array(array, computation_complexity);
         
         array[0] = 5;
         array[1] = 3;
+      
 
         for(int i=0; i < iterations; i++) {
             struct timespec start, end;
 
             //Migration benchmark only happens in odd iterations
-            bool do_migration = i % 2 == 1;
+            bool do_migration = (i % 2 == 0);
 
             // CODICE PER MIGRAZIONE
             const int nextCPU = get_next_cpu();
@@ -93,22 +104,24 @@ int main(int argc, char *argv[]) {
             
             //Elapsed time
             delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-            if(do_migration) total_migration_delays += delta_us;
-            else total_sequential_delays += delta_us;
-
-            if(i % (iterations / 10) == 0) {
-                printf(" %ld%%", i/(iterations/100));
-                sleep(0);
+            if(do_migration) { 
+                total_migration_delays += delta_us;
+                migration_iterations++;
+            }
+            else {
+                total_sequential_delays += delta_us;
+                normal_iterations++;
             }
         }
 
         //Compute the averages
-        double avg_migration = total_migration_delays / iterations;
-        double avg_sequential = total_sequential_delays / iterations;
+        double avg_migration = total_migration_delays / migration_iterations;
+        double avg_sequential = total_sequential_delays / normal_iterations;
 
         //Print them
-        printf("\navg_migration=%f, avg_sequential=%f\n",avg_migration,avg_sequential);
-        printf("- Delta: %f, %f%%\n", avg_migration - avg_sequential, (avg_migration - avg_sequential)*100/avg_sequential);
+        printf("\navg_migration=%.2fnms, avg_sequential=%.2fms",avg_migration,avg_sequential);
+        printf("\ntotal_migration=%d, total_sequential=%d", migration_iterations, normal_iterations);
+        printf("\n- Delta: %.2fms, %.2f%%\n", avg_migration - avg_sequential, (avg_migration - avg_sequential)*100/avg_sequential);
     }
     return EXIT_SUCCESS;
 }
